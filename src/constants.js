cea4d45f2fd8c5e841c473a92c63387e11488675
itzMeLucifer <devsab1997@gmail.com>
2023-04-17 20:23:55 +0530

    first

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const GET_REQUESTS = 'GET_REQUESTS'
 export const ADD_REQUEST  = 'ADD_REQUEST'
 export const UPDATE_REQUEST = 'UPDATE_REQUEST'
-export const API_URL = 'http://localhost:5000/api'
+export const API_URL = 'https://rms-mern-backend.onrender.com/api'
 export const config = {
     headers:{
         Authorization : localStorage.getItem('authToken')
